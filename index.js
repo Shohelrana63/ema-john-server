97fedb525beacfb9d26ac0c64862c850463dcb73
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-10-01 18:49:27 +0600

    connect database and create post endpoint

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqmks.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
 const port = 5000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const products = client.db("emaJohnStore").collection("products");
+
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        products.insertOne(product)
+            .then(result => {
+                console.log(result);
+            })
+    })
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
