dbe75ab775929447bfe5dfc7bd7fbccbf622c8d4
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-10-01 19:30:33 +0600

    insert bulk data many to the database

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
 
-const MongoClient = require('mongodb').MongoClient;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqmks.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 const app = express()
+
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -17,13 +19,14 @@ const port = 5000
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const products = client.db("emaJohnStore").collection("products");
+    const productsCollection = client.db("emaJohnStore").collection("products");
 
     app.post('/addProduct', (req, res) => {
-        const product = req.body;
-        products.insertOne(product)
+        const products = req.body;
+        productsCollection.insertMany(products)
             .then(result => {
-                console.log(result);
+                console.log(result.insertedCount);
+                res.send(result.insertedCount);
             })
     })
 });
