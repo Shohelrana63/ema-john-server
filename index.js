ed2b01579ac3363cfb10dcf0c8b4ed098fadc735
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-10-02 23:26:16 +0600

    duplicate data remove

--- index.js
+++ index.js
@@ -21,16 +21,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const productsCollection = client.db("emaJohnStore").collection("products");
     const ordersCollection = client.db("emaJohnStore").collection("orders");
-
+    console.log("database connected");
     app.post('/addProduct', (req, res) => {
         const products = req.body;
         productsCollection.insertOne(products)
             .then(result => {
                 console.log(result.insertedCount);
-                res.send(result.insertedCount);
+                res.send(result);
             })
     })
 
+  
     app.get('/products', (req, res) => {
         productsCollection.find({})
             .toArray((err, documents) => {
